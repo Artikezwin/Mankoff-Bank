3f35d94e0c303ec7140cad4c659021c9ab5517f4
Gregory <grisha101202@yandex.ru>
2022-12-20 21:30:20 +0300

    refactor code

--- src/main/java/ru/evsmanko/mankoff/service/GregoryServiceImpl.java
+++ src/main/java/ru/evsmanko/mankoff/service/GregoryServiceImpl.java
@@ -2,7 +2,7 @@ package ru.evsmanko.mankoff.service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.evsmanko.mankoff.entity.User;
 import ru.evsmanko.mankoff.repository.UserRepository;
@@ -12,19 +12,14 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class GregoryServiceImpl implements GregoryService {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public GregoryServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public void exportUserInJson(long id) {
         User user = userRepository.getUserById(id);
-
         File file = new File("jsons/user" + id + ".json");
         if (!file.exists()) {
             try {
@@ -33,12 +28,12 @@ public class GregoryServiceImpl implements GregoryService {
                 throw new RuntimeException(e);
             }
         }
-            try (FileWriter writer = new FileWriter(file, false)) {
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                writer.write(gson.toJson(user));
-                writer.flush();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+        try (FileWriter writer = new FileWriter(file, false)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            writer.write(gson.toJson(user));
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/ru/evsmanko/mankoff/testService/GregoryServiceTest.java
+++ src/test/java/ru/evsmanko/mankoff/testService/GregoryServiceTest.java
@@ -5,10 +5,12 @@ import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.evsmanko.mankoff.entity.User;
 import ru.evsmanko.mankoff.repository.UserRepository;
-import ru.evsmanko.mankoff.service.GregoryService;
 import ru.evsmanko.mankoff.service.GregoryServiceImpl;
 
 import java.io.BufferedReader;
@@ -16,26 +18,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class GregoryServiceTest {
 
+    @Mock
     private UserRepository userRepository;
-    private GregoryService gregoryService;
+    @InjectMocks
+    private GregoryServiceImpl gregoryService;
 
     @DisplayName("Тест на экспорт данных пользователя в json по id, ожидаемый результат получен")
     @Test
     public void testExportUserInJson() {
-        userRepository = Mockito.mock(UserRepository.class);
-        gregoryService = new GregoryServiceImpl(userRepository);
         long userId = 1;
         User user = new User(userId, "firstName", "lastName", "123345");
-        Mockito.when(userRepository.getUserById(userId)).thenReturn(user);
+        when(userRepository.getUserById(userId)).thenReturn(user);
         gregoryService.exportUserInJson(userId);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         File file = new File("jsons/user" + userId + ".json");
         StringBuilder stringBuilder = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            while(reader.ready()){
+            while (reader.ready()) {
                 stringBuilder.append(reader.readLine()).append("\n");
             }
             reader.close();
