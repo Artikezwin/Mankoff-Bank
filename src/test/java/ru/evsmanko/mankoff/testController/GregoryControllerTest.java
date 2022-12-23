93d8aa2aab5d650627a3cd2efdc5eb7f7be17915
Gregory <grisha101202@yandex.ru>
2022-12-23 21:38:11 +0300

    add test

--- src/test/java/ru/evsmanko/mankoff/testController/GregoryControllerTest.java
+++ src/test/java/ru/evsmanko/mankoff/testController/GregoryControllerTest.java
@@ -1,2 +1,23 @@
-package ru.evsmanko.mankoff.testController;public class GregoryControllerTest {
+package ru.evsmanko.mankoff.testController;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GregoryControllerTest {
+
+    @DisplayName("Тест на работу контроллера, ожидаемый результат получен")
+    @Test
+    public void testGregoryControllerReturn200WithExpectedUser() {
+        when()
+                .get("/gregory/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1),
+                        "firstName", equalTo("Евгений"),
+                        "lastName", equalTo("Манько"),
+                        "phone", equalTo("79166679083"));
+    }
 }
