d765c102a429d6c8152f180abb3d9f3bd9d73758
Darya Balandiuk <dasha.balandiuk2003@gmail.com>
2023-02-08 15:22:57 +0300

    hw 8 and hw 9

---
+++ src/main/java/ru/evsmanko/mankoff/controller/DaryaController.java
@@ -0,0 +1,53 @@
+package ru.evsmanko.mankoff.controller;
+
+mport lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.evsmanko.mankoff.configuration.CurrencyProperties;
+import ru.evsmanko.mankoff.service.DaryaServiceImpl;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("balance")
+public class DaryaController {
+
+    private final DaryaServiceImpl daryaService;
+    private final CurrencyProperties currencyProperties;
+
+    private double toUsd() {
+        return daryaService.debitAccountsBalance() / currencyProperties.getRUB();
+    }
+
+    private double toEur() {
+        return toUsd() / currencyProperties.getEUR();
+    }
+
+    @GetMapping("/currency/rub")
+    public ResponseEntity<Double> debitAccountsBalanceRub() {
+        if (daryaService != null) {
+            return new ResponseEntity<>(daryaService.debitAccountsBalance(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/currency/usd")
+    public ResponseEntity<Double> debitAccountsBalanceUsd() {
+        if (daryaService != null) {
+            return new ResponseEntity<>(toUsd(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/currency/eur")
+    public ResponseEntity<Double> debitAccountsBalanceEur() {
+        if (daryaService != null) {
+            return new ResponseEntity<>(toEur(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+}
