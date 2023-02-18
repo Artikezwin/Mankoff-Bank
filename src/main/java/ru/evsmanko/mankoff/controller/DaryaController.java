09883d6f9218ccf9e6467e6c86b5dc9472ffd31e
Gregory Golubev <grisha101202@yandex.ru>
2023-02-18 21:47:00 +0300

    finish hw13

--- src/main/java/ru/evsmanko/mankoff/controller/DaryaController.java
+++ src/main/java/ru/evsmanko/mankoff/controller/DaryaController.java
@@ -1,9 +1,10 @@
 package ru.evsmanko.mankoff.controller;
 
-mport lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.evsmanko.mankoff.configuration.CurrencyProperties;
 import ru.evsmanko.mankoff.service.DaryaServiceImpl;
